2d2c2ea21f2ed8db2fe4be9f3051ffe44bdd4d7f
Yegor Stepanov <ystepanoff@icloud.com>
2025-01-25 08:32:37 +0000

    Minimal PyTorch dataset class

--- train.py
+++ train.py
@@ -23,6 +23,19 @@ class DickinsonPoemsDataset(Dataset):
                 self.encoded_tokens.extend(encoding.ids)
             self.encoded_tokens.append(tokeniser.token_to_id("<eol>"))
 
+    def __len__(self):
+        return len(self.encoded_tokens) - self.seq_len
+
+    def __getitem__(self, idx):
+        x = torch.tensor(
+            self.encoded_tokens[idx : idx + self.seq_len],
+            dtype=torch.long,
+        )
+        y = torch.tensor(
+            self.encoded_tokens[idx + 1 : idx + self.seq_len + 1],
+            dtype=torch.long,
+        )
+        return x, y
+
 
 dataset = DickinsonPoemsDataset("data/dickinson_clean.txt")
-print(dataset.encoded_tokens)
