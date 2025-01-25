886c0093be59aca4cb9d92e8ea2a7749e9b7ddac
Yegor Stepanov <ystepanoff@icloud.com>
2025-01-25 08:28:44 +0000

    Preparing the dataset

--- train.py
+++ train.py
@@ -5,13 +5,24 @@ from torch.utils.data import Dataset
 
 class DickinsonPoemsDataset(Dataset):
     def __init__(self, data_path, seq_len=64):
+        self.encoded_tokens = []
+        self.seq_len = seq_len
+
         tokeniser = ByteLevelBPETokenizer()
         tokeniser.train(
             files=data_path,
             vocab_size=2000,
             min_frequency=2,
-            special_tokens=["<s>", "</s>", "<pad>", "<unk>", "<mask>"],
+            special_tokens=["<s>", "</s>", "<pad>", "<unk>", "<mask>", "<eol>"],
         )
+
+        with open(data_path, "r") as data_file:
+            for line in data_file:
+                line = line.strip("\n")
+                encoding = tokeniser.encode(line)
+                self.encoded_tokens.extend(encoding.ids)
+            self.encoded_tokens.append(tokeniser.token_to_id("<eol>"))
 
 
 dataset = DickinsonPoemsDataset("data/dickinson_clean.txt")
+print(dataset.encoded_tokens)
